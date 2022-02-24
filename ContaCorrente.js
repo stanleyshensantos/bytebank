2da193d3880a3952015a9c9f7564d6504f0e5df3
Staney Shen <stanley_shen@hotmail.com>
2022-02-24 20:38:35 -0300

    https://github.com/stanleyshensantos/bytebank

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,31 @@
+import {
+    Cliente
+} from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
+    _cliente;
+
     _saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+    get cliente (){
+        return this._cliente;
+    }
+
+    get saldo (){
+        return this._saldo;
+    }
+    constructor (agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -8,10 +33,15 @@ export class ContaCorrente {
             return valor;
         }
     }
-    depositar(valor){
-        if(valor <= 0){
-            return;    
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
         }
-        this._saldo += valor;  
+        this._saldo += valor;
+    }
+    transferir(valor, conta) {
+        conta.cidade = "São Paulo";
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- index.js
+++ index.js
@@ -2,26 +2,22 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 
-const cliente1 = new Cliente();
-cliente1.nome = "Stanley";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Stanley" , 11122233309);
+const cliente2 = new Cliente("Alice" , 8882223309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 8882223309;
-
-const contaCorrenteStanley = new ContaCorrente();
-contaCorrenteStanley.agencia = 1001;
+const contaCorrenteStanley = new ContaCorrente(1001 , cliente1);
 
 
-console.log(contaCorrenteStanley._saldo);
-contaCorrenteStanley.depositar(100);
-contaCorrenteStanley.depositar(100);
-contaCorrenteStanley.depositar(100);
-console.log(contaCorrenteStanley._saldo);
+contaCorrenteStanley.depositar(500);
+
+const conta2 = new ContaCorrente(102 , cliente2);
+new ContaCorrente(102 , cliente2);
+new ContaCorrente(102 , cliente2);
+new ContaCorrente(102 , cliente2);
+
+let valor = 200;
+contaCorrenteStanley.transferir(valor, conta2);
 
 
-const valorSacado = contaCorrenteStanley.sacar(50);
-console.log(valorSacado);
-console.log(contaCorrenteStanley);
+console.log(ContaCorrente.numeroDeContas);
 
