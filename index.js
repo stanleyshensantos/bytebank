0d0faf6a601d6e2d8ec201e20f95c04844553f5f
Staney Shen <stanley_shen@hotmail.com>
2022-02-28 12:10:38 -0300

    https://github.com/stanleyshensantos/bytebank

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+
 
 
 const cliente1 = new Cliente("Stanley" , 11122233309);
-const cliente2 = new Cliente("Alice" , 8882223309);
 
-const contaCorrenteStanley = new ContaCorrente(1001 , cliente1);
-
-
-contaCorrenteStanley.depositar(500);
-
-const conta2 = new ContaCorrente(102 , cliente2);
-new ContaCorrente(102 , cliente2);
-new ContaCorrente(102 , cliente2);
-new ContaCorrente(102 , cliente2);
-
-let valor = 200;
-contaCorrenteStanley.transferir(valor, conta2);
+const contaCorrenteStanley = new ContaCorrente(0, cliente1, 1001);
 
 
-console.log(ContaCorrente.numeroDeContas);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
+const conta = new Conta(0, cliente1, 1001);
+
+
+console.log(contaPoupanca);
+console.log(contaCorrenteStanley);
 
